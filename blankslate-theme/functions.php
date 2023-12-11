51e3faa0125f59df9bb1ef278b88544e621a26a9
manish457 <62796034+manish457@users.noreply.github.com>
2023-12-11 16:55:29 +0530

    blankslate-theme

--- blankslate-theme/functions.php
+++ blankslate-theme/functions.php
@@ -197,5 +197,15 @@ require get_theme_file_path( 'inc/theme-add-action.php' );
 
 
 
+//================= remove all update notification  =========================//
+function remove_core_updates(){
+global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates');
+add_filter('pre_site_transient_update_plugins','remove_core_updates');
+add_filter('pre_site_transient_update_themes','remove_core_updates');
 
+remove_action('load-update-core.php','wp_update_plugins');
+add_filter('pre_site_transient_update_plugins','__return_null');
+//=================== END: remove all update notification  ==================//
  
